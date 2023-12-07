7fd68fa6e52581ec23b03aa5d006948f5cbbec7d
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-12-07 16:36:12 +0530

    Added basic form structure and context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import UserInputsForForm from "./components/UserInputsForForm";
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='App'>
+      <div>Form Generator</div>
+      <div className='wrapper'>
+        <UserInputsForForm />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { GetForm } from "./context/formContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GetForm>
+      <App />
+    </GetForm>
   </React.StrictMode>
 );
